497cdf1fa1202302946004f296ead4ac0774da25
tylerwolverton <tylerwolvert@gmail.com>
2019-07-16 21:37:57 -0400

    Added matte shading
    Ch 7

--- main.cpp
+++ main.cpp
@@ -9,14 +9,33 @@
 #include "HitableList.h"
 #include "Camera.h"
 
+float randf01()
+{
+	return float(rand() % 100) / 100;
+}
+
+// Find point in unit cube and reject if not in sphere
+Vec3f GetRandomVectorInUnitSphere()
+{
+	Vec3f p;
+	do {
+		p = 2.0 * Vec3f(randf01(), randf01(), randf01()) - Vec3f(1, 1, 1);
+	} while (p.GetSquaredMagnitude() >= 1);
+
+	return p;
+}
+
 // Need to pass by value to avoid polluting input ray
 Vec3f DetermineColor(Rayf ray, Hitable* world)
 {
 	HitRecord_t rec;
-	if(world->Hit(ray, 0.0, FLT_MAX, rec))
+	if(world->Hit(ray, 0.001f, FLT_MAX, rec))
 	{ 
-		// Maps from -1 - 1 to 0 - 1 by adding 1 and dividing by 2
-		return 0.5f * Vec3f(rec.normal.x + 1, rec.normal.y + 1, rec.normal.z + 1);
+		// Target is a random point in the sphere centered on the point 1 unit
+		// away along the normal from the hit point, p
+		Vec3f target = rec.p + rec.normal + GetRandomVectorInUnitSphere();
+		// Return half intensity of the color found at reflected target location
+		return 0.5f * DetermineColor(Rayf(rec.p, target - rec.p), world);
 	}
 	else
 	{
@@ -53,9 +72,12 @@ int main()
 			Vec3f color(0, 0, 0);
 			for (int s = 0; s < nRaysPerPixel; s++)
 			{
-				// Convert rgb to a val between 0 and 1, randomly distributed in the pixel area to remove aliasing
-				float u = (float(i) + float(rand() % 100) / 100) / float(nCols);
-				float v = (float(j) + float(rand() % 100) / 100) / float(nRows);
+				// Convert rgb to a val between 0 and 1
+				// Also, randomly distribute the ray between this pixel and the next to remove aliasing
+				// The rand will add something onto the "beginning" of this pixel, then dividing by total number
+				// converts to a value between 0 and 1, eventually representing all the pixels for a row/column evenly spaced between 0 and 1
+				float u = (float(i) + randf01()) / float(nCols);
+				float v = (float(j) + randf01()) / float(nRows);
 
 				// Ray from origin to current pixel
 				Rayf ray(camera.GetRay(u, v));
@@ -66,7 +88,9 @@ int main()
 				color += DetermineColor(ray, world);
 			}
 
+			// Average the individual color values determined by each ray to blend into the final pixel color
 			color /= float(nRaysPerPixel);
+			color = Vec3f(sqrt(color.r), sqrt(color.g), sqrt(color.b));
 			Vec3i rgbScaled(255.9*color.r,
 							255.9*color.g,
 							255.9*color.b);
